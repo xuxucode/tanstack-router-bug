9b774e52348afacb79d7496f015c2daaa3d65fc5
xuxu <edward@picupmedia.com>
2025-03-21 18:15:55 +0800

    add reproduce code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
 import '../App.css'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
+function throwError() {
+  throw new Error('this is an error')
+}
+
+function handleClick() {
+  console.log('button clicked')
+  // Using the object destructure.
+  let { something } = throwError()
+  console.log('this message should not be printed')
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    <button onClick={handleClick}>Click</button>
   )
 }
